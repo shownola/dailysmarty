1ea0a0767be5f966c91a9c7b6cd7d228d6d4099b
SHERRY <swasieko@gmail.com>
2017-04-30 23:11:25 +0000

    Added the Friendly_Id gem for creating slugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430205942) do
+ActiveRecord::Schema.define(version: 20170430220922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "slug"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
